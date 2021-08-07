68d5e8919316ae70e4c53af2a18b4083b7bdbc55
asohora <88558699+asohora@users.noreply.github.com>
2021-08-07 16:13:51 +0100

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,39 @@
+function buttonH() {
+	document.getElementById("home").style.display = "flex";
+	document.getElementById("who").style.display = "none";
+	document.getElementById("privacy").style.display = "none";
+	document.getElementById("products").style.display = "none";
+	document.getElementById("contact").style.display = "none";
+	}
+function buttonW() {
+	document.getElementById("home").style.display = "none";
+	document.getElementById("who").style.display = "grid";
+	document.getElementById("privacy").style.display = "none";
+	document.getElementById("products").style.display = "none";
+	document.getElementById("contact").style.display = "none";
+	}
+
+function buttonPri() {
+	document.getElementById("home").style.display = "none";
+	document.getElementById("who").style.display = "none";
+	document.getElementById("privacy").style.display = "grid";
+	document.getElementById("products").style.display = "none";
+	document.getElementById("contact").style.display = "none";
+}
+
+function buttonPro() {
+document.getElementById("home").style.display = "none";
+document.getElementById("who").style.display = "none";
+	document.getElementById("privacy").style.display = "none";
+	document.getElementById("products").style.display = "grid";
+	document.getElementById("contact").style.display = "none";
+}
+
+function buttonC() {
+	document.getElementById("home").style.display = "none";
+	document.getElementById("who").style.display = "none";
+	document.getElementById("privacy").style.display = "none";
+	document.getElementById("products").style.display = "none";
+	document.getElementById("contact").style.display = "grid";
+
+}
